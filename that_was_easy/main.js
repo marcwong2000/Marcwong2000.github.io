092613b364f2b981ca265453ee0721d4cac6b70c
Marc Wong <marcwong2000@hotmail.com>
2017-02-18 08:28:21 +0000

    Create that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -3,12 +3,12 @@ function sayThatWasEasy() {
   thatWasEasy.play();
 }
 
-$("#easy").on("click", sayThatWasEasy);
+$("#YEEZY").on("click", sayThatWasEasy);
 
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event) {
   if (event.charCode == 32) {
-    $("#easy").trigger("click");
+    $("#Yeezy").trigger("click");
   }
 }
